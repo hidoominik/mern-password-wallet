5a47f57ad947ec27b60d0050c56fb775a42b3b78
hidoominik <dominik.choma.41@gmail.com>
2022-10-19 12:02:27 +0200

    components improvements add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-
-import {getPasswords} from './actions/passwords';
+import {BrowserRouter, Switch, Route, Routes} from 'react-router-dom';
 import lock from './images/lock.png'
 import Passwords from './components/Passwords/Passwords';
 import Form from './components/Form/Form';
-
+import Navbar from './components/Navbar/Navbar';
 import useStyles from './styles'
+import Home from './components/Home/Home';
 const App = () =>{
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(()=>{
-        dispatch(getPasswords())
-    },[dispatch]);
-
-
+    
     return(
-       <Container maxWidth="lg">
-        <AppBar className={classes.appBar} position='static' color="inherit">
-            <Typography variant='h2' align='center'> Password Wallet </Typography>
-            <img className={classes.heading} src={lock} alt='lock' height="60" />
-        </AppBar>
-        <Grow in>
-            <Container>
-                <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems='stretch' spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Passwords setCurrentId={setCurrentId}/>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                    </Grid>
-                </Grid>
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar/>
+                <Home/>
+                <Routes>
+                    <Route path='/' exac element={<Home />}/>
+                </Routes>
             </Container>
-        </Grow>
-
-       </Container>
+        
+        </BrowserRouter>
+      
     )
 }
 
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { AppBar,Avatar,Button,Toolbar,Typography } from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import useStyles from './styles';
+import lock from '../../images/lock.png';
+
+
+const Navbar= ()=> {
+    const classes = useStyles();
+    const user = null;
+    return(
+    <AppBar className={classes.appBar} position='static' color="inherit">
+        <div className={classes.brandContainer}>
+            <Typography variant='h2' component={Link} to="/" align='center'> Password Wallet </Typography>
+            <img className={classes.heading} src={lock} alt='lock' height="60" />
+
+        </div>
+        <Toolbar className={classes.toolbar}>
+            {user ? ( //This if there is logged in user, we display his avatar, nickname, and button to logout
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}> {user.result.name.charAt(0)}</Avatar>
+                    <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                    <Button variant='contained' className={classes.logout} color='secondary'>Logout</Button>
+                </div>
+            ):(
+                <Button variant='contained' className={classes.login} color='primary'>Login</Button>
+            )}
+        </Toolbar>
+    </AppBar> 
+    )
+    
+}
+
+export default Navbar
