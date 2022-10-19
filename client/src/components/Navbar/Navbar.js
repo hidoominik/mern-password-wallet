ecc4065f4c4ab5815796fa114e1bbd24ab985737
hidoominik <dominik.choma.41@gmail.com>
2022-10-19 22:28:35 +0200

    logout function, auth reducer

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { AppBar,Avatar,Button,Toolbar,Typography } from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import {Link, useLocation, useNavigate} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
 import useStyles from './styles';
 import lock from '../../images/lock.png';
 
 
 const Navbar= ()=> {
     const classes = useStyles();
-    const user = null;
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    
+    useEffect(()=>{
+        const token = user?.token;
+        //JWT... 
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
+
+    const logout =()=>{
+        dispatch({type: 'LOGOUT'});
+        navigate("/");
+        setUser(null);
+    }
+
     return(
     <AppBar className={classes.appBar} position='static' color="inherit">
         <div className={classes.brandContainer}>
@@ -20,7 +38,7 @@ const Navbar= ()=> {
                 <div className={classes.profile}>
                     {/* //<Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}> {user.result.name.charAt(0)}</Avatar> */}
                     <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                    <Button variant='contained' className={classes.logout} color='secondary'>Logout</Button>
+                    <Button variant='contained' className={classes.logout} onClick={logout} color='secondary'>Logout</Button>
                 </div>
             ):(
                 <Button variant='contained' component={Link} to="/Auth" className={classes.login} color='primary'>Login</Button>
